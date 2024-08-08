a928a1cd3a96683a694f157022564df45b96985f
Guilherme-14 <132360130+Guilherme-14@users.noreply.github.com>
2024-08-08 10:47:16 -0300

    Projeto Hotel

---
+++ src/main/java/com/example/entities/Hospede.java
@@ -0,0 +1,48 @@
+package com.example.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hospede")
+public class Hospede {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, length = 100)
+    private Long id_hospede;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String telefone;
+	
+	private String rua;
+	
+	private String cidade;
+
+	private String estado;
+	
+	private String numero;
+	
+	private String cep;
+	
+	private String cpf;
+	
+	@ManyToOne
+    @JoinColumn(name = "id_reserva")
+    private Reserva reserva;
+	
+}
